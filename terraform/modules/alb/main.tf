d16999cf828ea564630fd3678de4cd948516f6d6
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-24 17:18:53 +0530

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -56,19 +56,3 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-# Outputs for other modules (like CodeDeploy)
-output "alb_arn" {
-  value = aws_lb.this.arn
-}
-
-output "blue_tg_name" {
-  value = aws_lb_target_group.blue.name
-}
-
-output "green_tg_name" {
-  value = aws_lb_target_group.green.name
-}
-
-output "listener_arn" {
-  value = aws_lb_listener.http.arn
-}

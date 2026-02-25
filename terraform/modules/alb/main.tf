fb44db71ae1e570b98522de5f3cd1c1e8d68a135
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-25 13:50:08 +0530

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -2,7 +2,7 @@
 resource "aws_lb" "this" {
   name               = "${var.project_name}-alb"
   load_balancer_type = "application"
-  subnets = subnets = var.subnet_ids
+  subnets = var.subnet_ids
   security_groups    = [var.alb_sg_id]
 }
 
